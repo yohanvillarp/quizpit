99b7c7997feeb315eb9e066b761c9902ab7eff75
Yohan Nikel Villar Presentacion <yohannikel21@gmail.com>
2026-07-31 18:50:28 -0500

    refactor(studio): implementa feature-sliced design y limpia plantilla vite

--- apps/studio/vite.config.ts
+++ apps/studio/vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
